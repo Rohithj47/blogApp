0e192ad1236847c65e7ad2798c2be25ae87c8e55
Rohithj47 <rohithjanardhan98@gmail.com>
2022-05-22 22:28:11 +0530

    get comments

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -1,3 +1,19 @@
-var User = require('../models/user') 
-var Post = require('../models/post') 
-var Comment = require('../models/comment') 
+require("dotenv").config();
+const Comment = require("../models/comment");
+const Post = require("../models/post");
+const { body, validationResult } = require("express-validator");
+const jwt = require("jsonwebtoken");
+
+exports.comments = function (req, res) {
+  Comment.find()
+    .sort([["timestamp", "descending"]])
+    .exec((err, data) => {
+      if (err) return res.json(err);
+
+      let comments = data.filter(
+        (comment) => comment.postId === req.params.post_id
+      );
+
+      return res.json(comments);
+    });
+};
